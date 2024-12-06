2c5ec9990f6e62ae84b978882796cc33a7851c5e
Apeksha-22 <168848064+Apeksha-22@users.noreply.github.com>
2024-12-06 09:43:05 +0000

    conatiner config done

--- basic/test.js
+++ basic/test.js
@@ -12,3 +12,20 @@ let accountState;
 //const ke varible ko change nhi kar sakte hai let or var ek jaise hai inhe change kar sakte hai or inme se koi ek hi use karna chaiye
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
 // jab varible ke declare kar dete hai or value nhi dete hai toh undefined aata hai
+
+// alert(1 + 2) not use because we use node js not browser
+
+// MDN and tc39 for doucmentation reads
+
+/* DataTypes
+numbers => 2 power 53
+string => ""
+bigint => bade numbers ke liye
+boolean => true/false
+null => standalone value koi value nhi hai
+undefined => koi value nhi di hai
+
+object
+null => typeof null is object
+undefined => undefined
+*/
